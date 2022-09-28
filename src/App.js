ba57b46f955559e6bdbe9848b2866246a778cca6
Dolores Perez Laborda <doloresperezlaborda@gmail.com>
2022-09-28 12:20:27 -0300

    se agrega lib

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import OtraPrueba from "./component/OtraPrueba";
 import Prueba from "./component/Prueba";
+import { version } from "./autobuild_version";
 
 function App() {
+  console.log("que onda", version);
   return (
     <div className="App">
       <Prueba />

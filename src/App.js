e84fa44910915549537c96e71860f0a1a6bc1d9e
Dolores Perez Laborda <doloresperezlaborda@gmail.com>
2022-09-28 12:37:46 -0300

    remove stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
 import OtraPrueba from "./component/OtraPrueba";
 import Prueba from "./component/Prueba";
-import { version } from "./autobuild_version";
 
 function App() {
-  console.log("que onda", version);
   return (
     <div className="App">
       <Prueba />
